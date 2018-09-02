b34dab7f091c8a6276258072c0a8f233669f366f
Jeff Schwab <jeff@feltapp.com>
2018-09-01 19:08:39 -0700

    commit

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(":P");
+  response.send(200);
 });
 
 app.get('/xpath', function(request, response) {
   response.send({"xpath":"//div[@class='content']/table/tr/td[@class='sres']/table[@class='tresults  ']/tr/td/div[a]"});
 });
+
+"div[@class='content']/table/tr/td[@class='sres']/table[@class='tresults  ']/tr/td/div[a]"
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
