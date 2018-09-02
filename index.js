ffef2bbb4df0636e25f0d1c141df3b7f818e7589
Jeff Schwab <jeff@feltapp.com>
2018-09-01 19:14:06 -0700

    Stuff

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  
+  response.send(200);
 });
 
 app.get('/xpath', function(request, response) {
